0d534ff69c261eb5d404f92543404410f1aa9bcc
yeetyeet <pojaluci@easy-apps.info>
2019-12-28 20:45:50 -1000

    save yay

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,9 +1,11 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class LambdaButtons {
 	private JFrame window = new JFrame();
@@ -17,12 +19,28 @@ public class LambdaButtons {
 		window.add(addNumbers);
 		window.add(randNumber);
 		window.add(tellAJoke);
+		addNumbers.addActionListener(e->add());
+		randNumber.addActionListener(e->rand());
+		tellAJoke.addActionListener(e->joke());
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
 		
 		window.setVisible(true);
 		window.pack();
+	}
+	public void add() {
+		int a = Integer.parseInt(JOptionPane.showInputDialog("Enter the first number:"));
+		int b = Integer.parseInt(JOptionPane.showInputDialog("Enter the second number:"));
+		System.out.println(a+" + "+b+" = "+(a+b));
+	}
+	public void rand() {
+		int r = new Random().nextInt(50)+1;
+		System.out.println("You found a randomly generated number: "+r+"\nThank God you didn't eat it.");
+	}
+	public void joke() {
+		String a = "What is the best thing about Switzerland?\n\nI don't know, but the flag is a huge plus.";
+		System.out.println(a);
 	}
 	
 	public static void main(String[] args) {
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -3,27 +3,71 @@ package _01_Lambda_Methods;
 public class LambdaMethods {
 	public static void main(String[] args) {
 		// 1. Look at the SpecialPrinter function interface.
-	
+
 		// Here is an example of calling the printCustomMessage method with a lambda.
 		// This prints the passed in String 10 times.
-		printCustomMessage((s)->{
-			for(int i = 0; i < 10; i++) {
+		printCustomMessage((s) -> {
+			for (int i = 0; i < 10; i++) {
 				System.out.println(s);
 			}
 		}, "repeat");
-		
-		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
-		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
-		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
-		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+
+		// 2. Call the printCustonMessage method using a lambda so that the String
+		// prints backwards.
+		String a = "The String";
+		printCustomMessage(s -> {
+			String b = "";
+			for (int i = 0; i < a.length(); i++) {
+				b = b + a.charAt(a.length() - i - 1);
+			}
+			System.out.println(b);
+		}, a);
+		// 3. Call the printCustonMessage method using a lambda so that the String
+		// prints with a mix between upper an lower case characters.
+		printCustomMessage(s -> {
+			String b = "";
+			for (int i = 0; i < a.length(); i++) {
+				char temp;
+				if(i%2==0) {
+					temp=Character.toLowerCase(a.charAt(i));
+				}else {
+					temp=Character.toUpperCase(a.charAt(i));
+				}
+				b=b+temp;
+			}
+			System.out.println(b);
+		}, a);
+		// 4. Call the printCustonMessage method using a lambda so that the String
+		// prints with a period in between each character.
+		printCustomMessage(s->{
+			String b="";
+			for (int i = 0; i < a.length(); i++) {
+				b=b+a.charAt(i)+".";
+			}
+			System.out.println(b);
+		}, a);
+		// 5. Call the printCustonMessage method using a lambda so that the String
+		// prints without any vowels
+		char[] vowels= {'a','e','i','o','u','y'};
+		printCustomMessage(s->{
+			String b="";
+			boolean isTrue=true;
+			for (int i = 0; i < a.length(); i++) {
+				isTrue=true;
+				for (int j = 0; j < vowels.length; j++) {
+					if(a.charAt(i)==vowels[j]) {
+						isTrue=false;
+					}
+				}
+				if(isTrue) {
+					b=b+a.charAt(i);
+				}
+			}
+			System.out.println(b);
+		}, a);
 	}
-	
+
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
 		sp.printSpecial(value);
 	}
 }
-	
